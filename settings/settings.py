35c7b63f9e8bc25133ff698f45710477f7168059
Lizard670 <monarcasemtempo@gmail.com>
2026-06-07 03:46:13 -0300

    Instalação do bootstrap e modelo sb-admin

--- settings/settings.py
+++ settings/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'frontend',
 
     'rest_framework',
+    'bootstrap5'
 ]
 
 REST_FRAMEWORK = {
@@ -75,7 +76,7 @@ ROOT_URLCONF = 'settings.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -136,3 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
