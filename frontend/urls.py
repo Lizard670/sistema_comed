4ee8a5b663027e3b9d23b3a6b22390aa73378354
Lizard670 <monarcasemtempo@gmail.com>
2026-06-07 03:59:57 -0300

    Implementação inicial das rotas do frontend
    + Criação das views e caminhos para as páginas inicial, logar, registrar e resetar a senha
    + Inclusão dos urls do frontend no arquivo de urls principal

---
+++ frontend/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.pagina_inicial, name="index"),
+    path("logar", views.pagina_logar, name="logar"),
+    path("registrar", views.pagina_registrar, name="registrar"),
+    path("resetar_senha", views.pagina_resetar_senha, name="resetar_senha"),
+]
--- frontend/views.py
+++ frontend/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def pagina_inicial(request):
+    return render(request, "index.html")
+
+def pagina_logar(request):
+    return render(request, "logar.html")
+
+def pagina_registrar(request):
+    return render(request, "registrar.html")
+
+def pagina_resetar_senha(request):
+    return render(request, "resetar_senha.html")
