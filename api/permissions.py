cf921f6d1d482a2c44d2290267eedf13c776866a
ix5025887-lgtm <ix5025887@gmail.com>
2026-06-13 16:11:02 -0300

    permissões basicas

--- api/permissions.py
+++ api/permissions.py
@@ -1,4 +1,6 @@
-from rest_framework.permissions import BasePermission, SAFE_METHODS
+from rest_framework.permissions import BasePermission
 
 class IsAtendente(BasePermission):
-    
+
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated
