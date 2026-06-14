1fce932e7057ddf9de02b3db01c06e3c1b25de39
pedro3z0 <pedrovictus@gmail.com>
2026-06-14 17:37:50 -0300

    API: Adição das rotas em singular

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,20 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import UsuarioViewSet, CursoViewSet, TurmaViewSet, AlunoViewSet, ProntuarioViewSet, DeclaracaoViewSet
+from .views import (
+    UsuarioViewSet,
+    CursoViewSet,
+    TurmaViewSet,
+    AlunoViewSet,
+    ProntuarioViewSet,
+    DeclaracaoViewSet,
+    UsuarioDetailView,
+    CursoDetailView,
+    TurmaDetailView,
+    AlunoDetailView,
+    ProntuarioDetailView,
+    DeclaracaoDetailView
+)
 
 router = DefaultRouter()
 
@@ -14,4 +27,10 @@ router.register(r'declaracoes', DeclaracaoViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('usuario/<int:pk>/', UsuarioDetailView.as_view(), name='usuario'),
+    path('curso/<int:pk>/', CursoDetailView.as_view(), name='curso'),
+    path('turma/<int:pk>/', TurmaDetailView.as_view(), name='turma'),
+    path('aluno/<int:pk>/', AlunoDetailView.as_view(), name='aluno'),
+    path('prontuario/<int:pk>/', ProntuarioDetailView.as_view(), name='prontuario'),
+    path('declaracao/<int:pk>/', DeclaracaoDetailView.as_view(), name='declaracao')
 ]
